ceb2cb16c3d0b72abe7ac2b3e1f723eac91f5bae
Mels94 <mels.saribekyan@mail.ru>
2019-11-29 17:36:49 +0400

    change

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
-    // Add minus icon for collapse element which is open by default
-    $(".collapse.show").each(function(){
-        $(this).prev(".minus_plus").find(".fa").addClass("fa-minus").removeClass("fa-plus");
-    });
-
     // Toggle plus minus icon on show hide of collapse element
-    $(".collapse").on('show.bs.collapse', function(){
+/*    $(".collapse").on('show.bs.collapse', function(){
         $(this).prev(".minus_plus").find(".fa").removeClass("fa-plus").addClass("fa-minus");
     }).on('hide.bs.collapse', function(){
+        $(this).prev(".minus_plus").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+    });*/
+
+
+
+    $(".collapse").on('show.bs.collapse', function(){
+        $(this).prev(".minus_plus").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+        console.log('test', $(this))
+
+    }).on('hide.bs.collapse', function(){
+
         $(this).prev(".minus_plus").find(".fa").removeClass("fa-minus").addClass("fa-plus");
     });
 });
