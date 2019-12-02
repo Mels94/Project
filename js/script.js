10a7278bf26da367f694460027217453b2063b03
Mels94 <mels.saribekyan@mail.ru>
2019-12-02 18:10:14 +0400

    change

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    var count = 0
     // Toggle plus minus icon on show hide of collapse element
     $(".collapse").on('show.bs.collapse', function(){
         $(this).prev(".minus_plus").find(".fa").removeClass("fa-plus").addClass("fa-minus");
@@ -6,5 +7,18 @@ $(document).ready(function(){
         $(this).prev(".minus_plus").find(".fa").removeClass("fa-minus").addClass("fa-plus");
     });
 
-
+    $(".collapse-parent").on('click', function(){
+        if (count%2 == 0){
+            $(this).find(".fa").removeClass("fa-plus").addClass("fa-minus");
+        }
+        else{
+            $(this).find(".fa").removeClass("fa-minus").addClass("fa-plus");
+            $(".collapse").on('hide.bs.collapse', function(){
+                console.log("aaaaaaa")
+                $(this).find(".fa").removeClass("fa-minus").addClass("fa-plus");
+                $(this).find(".collapse").removeClass("show")
+            });
+        }
+        count++
+    });
 });
