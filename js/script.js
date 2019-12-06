33827d5d6ad76435d248ea1bc61cabab7126c9e1
Mels94 <mels.saribekyan@mail.ru>
2019-12-06 18:02:31 +0400

    change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var count = 0;
+/*    var count = 0;
     // Toggle plus minus icon on show hide of collapse element
     $(".collapse").on('show.bs.collapse', function () {
         $(this).prev(".minus_plus").find(".fa").removeClass("fa-plus").addClass("fa-minus");
@@ -18,8 +18,19 @@ $(document).ready(function () {
             });
         }
         count++;
-    });
+    });*/
 
+
+$(document).on('click','.plusMinus',function () {
+    if ($(this).attr('accesskey') === '1'){
+        $(this).attr('accesskey',0);
+        $(this).attr('src','images/b_minus.png');
+    }else {
+        $(this).attr('accesskey',1);
+        $(this).attr('src','images/b_plus.png');
+    }
+    console.log($(this).attr('accesskey'));
+});
 
 var nav = [
     {name:"Databases", img:"images/s_db.png", title: "Databases"},
@@ -50,6 +61,93 @@ nav.forEach((i)=>{
     a.innerHTML += i.name;
 });
 
+
+var collapse = [
+    {
+        firstname:"Registracia",
+        id:"demoout",
+        child:[
+            {
+                middlename:"User",
+                id:"demoin",
+                children:[
+                    {lastname:"Karen"},
+                    {lastname:"Lilit"},
+                    {lastname:"Arman"}
+                ]
+            },
+            {
+                middlename:"User_ID",
+                id:"demoin1",
+                children: [
+                    {lastname:"2"},
+                    {lastname:"5"},
+                    {lastname:"13"}
+                ]
+            },
+            {
+                middlename:"Friend_ID",
+                id:"demoin2",
+                children: [
+                    {lastname:"12"},
+                    {lastname:"8"},
+                    {lastname:"14"}
+                ]
+            }
+        ]
+    }
+];
+collapse.forEach((i) => {
+/*    let col = document.getElementById("collapse");
+    let div = document.createElement("div");
+    div.setAttribute('class', 'collapse-group');
+    col.appendChild(div);
+    let div1 = document.createElement("div");
+    div1.setAttribute('class', 'minus_plus');
+    div.appendChild(div1);
+    let a = document.createElement("a");
+    a.setAttribute('class', 'btn px-0');
+    a.setAttribute('data-toggle', 'collapse');
+    a.setAttribute('data-target', i.id);
+    div1.appendChild(a);
+    let img1 = document.createElement("img");
+    img1.setAttribute('src', 'images/b_plus.png');
+    img1.setAttribute('alt', 'plus');
+    img1.setAttribute('title', 'Expand/Collapse');
+    img1.setAttribute('class', 'plusMinus');
+    img1.setAttribute('accesskey', '1');
+    a.appendChild(img1);
+    let img2 = document.createElement("img");
+    img2.setAttribute('src', 'images/s_db.png');
+    img2.setAttribute('alt', 'db');
+    img2.setAttribute('title', 'Database operations');
+    a.appendChild(img2);
+    a.innerHTML += i.firstname;
+
+    console.log(i);
+    console.log(i.child);
+
+    for (let j = 0; j < i.child.length; j++) {
+        console.log(i.child[j]);
+
+        let div_in = document.createElement("div");
+        div_in.setAttribute('class', 'collapse in');
+        div_in.setAttribute('id', i.id);
+        col.appendChild(div_in);
+        let div_group = document.createElement("div");
+        div_group.setAttribute('class', 'collapse-group');
+        div_in.appendChild(div_group);
+
+        //div_group.appendChild(div1);
+
+
+
+        for (let k = 0; k < i.child[j].children.length; k++) {
+            console.log(i.child[j].children[k]);
+        }
+    }*/
+
+});
 
 
 
